b3ecfb36430171c30ad5ebc55da536231e6918ad
vatsaashwin <aashwinvats@GMAIL.com>
2019-02-22 17:45:51 -0800

    adding binary search tree

---
+++ DataStructures/cs261/binarySearchTree/BinaryTree/BinaryTree/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BinaryTree
+//
+//  Created by AASHWIN VATS on 22/02/19.
+//  Copyright © 2019 AASHWIN VATS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

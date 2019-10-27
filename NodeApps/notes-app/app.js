c40254712236c735310955e0172cb571e8aca253
vatsaashwin <aashwinvats@GMAIL.com>
2019-10-27 02:19:17 -0700

    updating with yargs

--- NodeApps/notes-app/app.js
+++ NodeApps/notes-app/app.js
@@ -1,9 +1,61 @@
 // npm module: validator
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const notes = getNotes();
-console.log(notes);
-// test validator package
-// chain things together
-console.log(chalk.cyan.inverse.bold("Success!"));
+// Customise yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  descibe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Body of note",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(argv) {
+    console.log("Title: ", argv.title);
+    console.log("Body: ", argv.body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function() {
+    console.log("Removing a note!");
+  }
+});
+
+// Create list notes command
+yargs.command({
+  command: "list",
+  describe: "list existing note",
+  //   builder:
+  handler: function() {
+    console.log("Listing out all notes!");
+  }
+});
+
+// Create command for reading the note
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function() {
+    console.log("Reading a note!");
+  }
+});
+
+// add,remove,read, list data
+// console.log(yargs.argv);
+yargs.parse();

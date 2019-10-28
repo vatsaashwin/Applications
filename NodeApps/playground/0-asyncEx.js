9e52388f153937da8a7fd1710a9fc35befa275f3
vatsaashwin <aashwinvats@GMAIL.com>
2019-10-27 17:58:54 -0700

    playground:demo of async node+ initial commit weather-app

---
+++ NodeApps/playground/0-asyncEx.js
@@ -0,0 +1,34 @@
+console.log("Starting...");
+
+setTimeout(() => {
+  console.log("2 second timer");
+}, 2000);
+
+setTimeout(() => {
+  console.log("0 second timer");
+}, 0);
+
+console.log("Stopping....");
+
+// Why does 0 second timer print after Stopping...?
+// Lessions learned:
+// How async node works:
+
+// 1. Call stack:
+// main() --> log() --> remove log() -->**setTimeout([])--> moves to Node APIs
+// --> ***setTimeout()--> moves to Node APIs-->we are left with only main()
+// --> log('stopping') --> removes log --> removes main-->++
+// -->from callback Q: Callback( 0 sec)--> log('0 sec)--> remove log
+// --> from callback Q: Callback( 2 sec) --> log('2 sec')--> removes log
+
+// Node APIs:
+// **setTimeout( 2 sec)--> ***setTimeout( 0 sec)--> moves to Callback Queue
+
+// Callback Queue:
+//***Callback (0 sec)-->moves to call stack after main() is removed-->
+// **Callback (2 sec)--> moves to call stack when callback 0 is removed-->
+
+// Event Loop:
+// Looks at Call stack and Callback Queue
+// If the Call stack is empty, it moves the fn from callback Q to Call Stack
+// If not,waits.

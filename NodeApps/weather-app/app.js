24a56c8c133ceb10955b1356dd66935be2229527
vatsaashwin <aashwinvats@GMAIL.com>
2019-11-17 03:10:23 -0800

    re-factoring

--- NodeApps/weather-app/app.js
+++ NodeApps/weather-app/app.js
@@ -1,41 +1,25 @@
 const request = require("request");
 const chalk = require('chalk')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'https://api.darksky.net/forecast/63581c7e05d248215dafc9789acb407d/37.8267,-122.4233?units=si'
+const address = process.argv[2]
+if (address) {
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return console.log(error)
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+      console.log(location)
+      console.log(forecastData)
+    })
+  })
+} else {
+  console.log('Please provide address...')
+}
 
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log(chalk.red.inverse.bold('Unable to connect to weather service!'))
-//   } else if (response.body.error) {
-//     console.log('Unable to find location')
-//   } else {
-//     console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain. ")
-//   }
-// })
-
-// Forward Geocoding
-// Address-> API converts to lat and long -> Weather
-
-// const urlGeo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidmF0c2FhYXNod2luIiwiYSI6ImNrMmEzeWxjeTF3ODMzYnBlY3d2bmEzbDIifQ.ZUoV1zzvGkN3y6PLoyGxSw'
-
-// request({ url: urlGeo, json: true }, (error, response) => {
-//   if (error) {
-//     console.log(chalk.red.inverse.bold('Unable to connect to geoLocation service!'))
-//   } else if (response.body.features.length == 0) {
-//     console.log('Unable to match location. Try another search!')
-//   } else {
-//     const longitude = response.body.features[0].center[0]
-//     const latitude = response.body.features[0].center[1]
-//     console.log(longitude, latitude)
-//   }
-// })
-
-
-
-geocode('Portland', (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
 
 
--- NodeApps/weather-app/utils/geocode.js
+++ NodeApps/weather-app/utils/geocode.js
@@ -2,17 +2,17 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidmF0c2FhYXNod2luIiwiYSI6ImNrMmEzeWxjeTF3ODMzYnBlY3d2bmEzbDIifQ.ZUoV1zzvGkN3y6PLoyGxSw'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         }
         else {
             callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
